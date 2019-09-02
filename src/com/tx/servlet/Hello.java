b3c89d9212d1949634611d04a0244be63d3bd181
xia.tian <xia.tian@dxc.com>
2019-09-02 12:19:05 +0800

    first commit

---
+++ src/com/tx/servlet/Hello.java
@@ -0,0 +1,83 @@
+package com.tx.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Hello extends HttpServlet {
+
+    public Hello() {
+        super();
+    }
+
+    public void destroy() {
+        super.destroy(); 
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("hello world !  This is ");
+        out.print(this.getClass());
+        out.println(", using the GET method!");
+        out.println("</BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+        System.out.println("doGet successfully");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
+        /**
+         * Ω” ’json
+         */
+        BufferedReader reader = request.getReader();
+        String json = reader.readLine();
+        System.out.println(json);
+        reader.close();
+
+        /**
+         * ∑µªÿjson
+         */
+        PrintWriter out = response.getWriter();
+     //   out.write("{\"status\":200,\"description\":\"create roles successfully\", \"intput info:\"} ");
+        out.write("{\"status_code\":200,\"description\":\"create roles successfully\", \"intput parameters\":");
+        out.write(json);
+        out.write("} \r\n");
+        out.close();
+
+    
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+//        out.println("<HTML>");
+//        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+//        out.println("  <BODY>");
+//        out.print("  hello world!  This is ");
+//        out.print(this.getClass());
+//        out.println(", using the POST method");
+//        out.println("  </BODY>");
+//        out.println("</HTML>");
+//        out.flush();
+//        out.close();
+    }
+
+}
---
+++ src/com/tx/servlet/Workflow.java
@@ -0,0 +1,78 @@
+package com.tx.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Workflow extends HttpServlet {
+	private String message;
+
+	  public void init() throws ServletException
+	  {
+	      // 执行必需的初始化
+	      message = "Hello World";
+	  }
+
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+				throws ServletException, IOException {
+			
+			// set response headers
+			response.setContentType("text/html");
+			response.setCharacterEncoding("UTF-8");
+			
+			// create HTML form
+			PrintWriter writer = response.getWriter();
+			writer.append("<!DOCTYPE html>\r\n")
+				  .append("<html>\r\n")
+				  .append("		<head>\r\n")
+				  .append("			<title>Form input</title>\r\n")
+				  .append("		</head>\r\n")
+				  .append("		<body>\r\n")
+				  .append("			<form action=\"workflow\" method=\"POST\">\r\n")
+				  .append("				Enter your name: \r\n")
+				  .append("				<input type=\"text\" name=\"user\" />\r\n")
+				  .append("				<input type=\"submit\" value=\"Submit\" />\r\n")
+				  .append("			</form>\r\n")
+				  .append("		</body>\r\n")
+				  .append("</html>\r\n");
+		}
+		@Override
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+				throws ServletException, IOException {
+			String user = request.getParameter("user");
+			
+			response.setContentType("text/html");
+			response.setCharacterEncoding("UTF-8");
+			
+			// create HTML response
+			PrintWriter writer = response.getWriter();
+			writer.append("<!DOCTYPE html>\r\n")
+				  .append("<html>\r\n")
+				  .append("		<head>\r\n")
+				  .append("			<title>Welcome message</title>\r\n")
+				  .append("		</head>\r\n")
+				  .append("		<body>\r\n");
+			if (user != null && !user.trim().isEmpty()) {
+				writer.append("	Welcome " + user + ".\r\n");
+				writer.append("	A workflow instanse has been created.\r\n");
+			} else {
+				writer.append("	You did not entered a name!\r\n");
+			}
+			writer.append("		</body>\r\n")
+				  .append("</html>\r\n");
+		}	
+	  
+	  public void destroy()
+	  {
+	      // 什么也不做
+	  }
+}
